3488a30848765ea9ce0b04850cf188b152d25623
lunar-luna-dmn <md3508@nyu.edu>
2024-09-17 00:49:00 -0400

    Add files via upload

---
+++ Week-2/typewriter-v1/script.js
@@ -0,0 +1,8 @@
+let button=document.getElementById("help-button");
+
+//listen for event clicking, and do something
+
+button.addEventListener("click", ()=>{
+    //console.log("helppppp");
+    alert("Never mind. You are fine.");
+})
